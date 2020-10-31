9aecf73cfde5a04d4e67a7dd102f451c3b033105
sayaka.tanaka <syk.851144436121@gmail.com>
2020-10-31 09:51:41 +0000

    kadai-microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(id: :desc).page(params[:page])
-    @favorites = @user.favorites.order(id: :desc).page(params[:page])
     counts(@user)
   end
 
